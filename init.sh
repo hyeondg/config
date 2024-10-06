27231b53c809d9cb11010f9390d93bf7525ee1d2
Hyeondong Lee <info@hyeondg.org>
2024-10-06 22:16:58 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -321,8 +321,8 @@ elif [[ "$OS" == "Linux" ]]; then
   git config --global credential.helper cache
 
   # Install vimplug
-  sh -c `curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`
-
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 popd >/dev/null
